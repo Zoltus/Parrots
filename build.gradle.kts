e9c81fed33210456d99d38377695a2335cd74df9
Zoltus <zoltus@outlook.com>
2025-06-28 12:06:20 +0300

    chore: update kotlin to 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.2.0"
     java
     id("com.gradleup.shadow") version "8.3.3"
 }
@@ -21,9 +21,9 @@ dependencies {
     implementation("dev.jorel:commandapi-bukkit-shade:10.1.0")
     implementation("org.bstats:bstats-bukkit:3.1.0")
 
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.2.0")
     compileOnly("org.spigotmc:spigot-api:1.21.6-R0.1-SNAPSHOT")
     compileOnly("com.comphenix.protocol:ProtocolLib:5.3.0")
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
     compileOnly("org.xerial:sqlite-jdbc:3.45.3.0")         // SQLite
     compileOnly("org.jetbrains.exposed:exposed-core:1.0.0-beta-2")
     compileOnly("org.jetbrains.exposed:exposed-jdbc:1.0.0-beta-2")
